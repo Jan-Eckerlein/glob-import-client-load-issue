13047f879b6787082db344fd8b7db35b91823da7
Jan Eckerlein <jan.eckerlein@fkt42.de>
2023-06-03 20:43:19 +0200

    Showcase issue:

---
+++ src/components/Counter.svelte
@@ -0,0 +1,18 @@
+<script>
+	export let count = 0;
+</script>
+
+<section>
+	<h1>Counter</h1>
+	<p>Count: {count}</p>
+	<button on:click={() => count++}>Increment</button>
+	<button on:click={() => count--}>Decrement</button>
+</section>
+
+<style>
+	section {
+		border: 1px solid #ccc;
+		padding: 0.5rem;
+		margin: 0.5rem;
+	}
+</style>
---
+++ src/components/TextField.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let value = '';
+</script>
+
+<section>
+	<h1>TextField</h1>
+	<input type="text" bind:value />
+	<p>Value: {value}</p>
+</section>
+
+<style>
+	section {
+		border: 1px solid #ccc;
+		padding: 0.5rem;
+		margin: 0.5rem;
+	}
+</style>
